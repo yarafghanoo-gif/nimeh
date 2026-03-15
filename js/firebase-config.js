63c3bb33581257f6ee27ee3befc40939b39e3a71
yarafghanoo-gif <yarafghanoo@gmail.com>
2026-03-15 23:31:16 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
