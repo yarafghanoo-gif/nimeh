660d467849170a53d43d8723ac1d3fe322bd3b85
yarafghanoo-gif <yarafghanoo@gmail.com>
2026-03-15 01:30:14 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -13,6 +13,9 @@ const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
 export const googleProvider = new GoogleAuthProvider();
